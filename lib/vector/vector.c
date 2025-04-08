c474514c32e08e6718e3bf0f85416b244ae91730
Lowe Andaya <kainken69@gmail.com>
2025-04-08 12:57:03 +0800

    first finishup

--- lib/vector/vector.c
+++ lib/vector/vector.c
@@ -6,108 +6,95 @@
 #include <stdlib.h>
 #include <string.h>
 
-int vect_auto_grow = 0;
+#include "../loggers/src/log.h"
+#include "../verbose/verbose.h"
+
+#define VV_NAME "vector|vec"
+#define VV_GET FILTERING_ENABLED ? is_filtered_with(VV_NAME) : true
+#define VV_ IS_VERBOSE ? VV_GET : false;
+bool is_verbose_local(){return VV_}
+#define VV if (is_verbose_local())
 
 /**
+int vect_auto_grow = 0;
  * State to automatically grow vectors
  **/
-void vect_set_auto_grow(int grow) { vect_auto_grow = grow; }
 
 vect_t *vect_alloc(size_t size, size_t cap) {
-  vect_t *vect = malloc(VECT_T_SIZE + size * cap);
+  VV log_info("vector: allocating vector size[%zu] cap[%zu]", size, cap);
+  size_t newsize = sizeof(vect_t) + (size * cap);
+  VV log_info("vector: allocating [%zu]", newsize);
+  assert(newsize > 0);
+  vect_t *vect = malloc(newsize);
   *vect = (vect_t){.cap = cap, .size = size, .len = 0}; // Initialize vector
+  VV log_info("vector: finished allocating vector[%p]", vect);
   return vect;
 };
 
 vect_t *vect_grow(vect_t *vect, size_t count) {
-  vect_t *nvect =
-      realloc(vect, +VECT_T_SIZE + (vect->size * (vect->cap + count)));
+  VV log_debug("vect: growing %zu count", count);
+  size_t new_capacity = vect->cap + count;
+  size_t new_storage = VECT_T_SIZE + (vect->size * new_capacity);
+  assert(new_storage > vect->cap * vect->size);
+  vect_t *nvect = realloc(vect, new_storage);
+
   if (nvect == NULL)
     return NULL;
-  nvect->cap += count;
+  VV log_debug("vect: grown %zu count sucessful", count);
+
+  VV log_debug("vect: cleaning up the memory");
+  char *mem = vect_end(nvect);
+  size_t diff = count * vect->size;
+  // memset
+  memset(mem, '\0', diff);
+  printf("\nDo something like this\n");
+
+  for (int i = 0; i < nvect->cap; i++) {
+    printf("%d ", *vect_get_as(int, vect, i));
+  }
+  nvect->cap = new_capacity;
   return nvect;
 };
 
 size_t vect_grow_if_needed(vect_t *vect) {
-  if (vect_auto_grow == 1 && vect->len + 1 > vect->cap) {
-    puts("GROW IS NEEDED");
-    if (!vect_grow(vect, 1))
+  if ( vect->len + 1 > vect->cap) {
+    VV log_info("vector: Needs to grow");
+    if (!vect_grow_self(vect, 1))
       return -1;
   }
   return 0;
 }
 
-
-
 // TODO:  Make a vecor to be pushed or append per value
 // TODO: allow arrays to be used as the vector contents
-
-void printIntArray(vect_t *vec, int len) {
-  printf("[ ");
-  for (int i = 0; i < len; i++) {
-    printf("%d ", ((int *)vect_first(vec))[i]);
-  }
-  printf("]\n");
-};
-
+// TODO move
 void *vect_move_i(vect_t *vect, size_t i) {
+  VV log_info("vector: Moving vector for %zu back", i);
+  VV log_debug("vector: Moving vector(%p)", vect);
   void *moved =
       memmove(vect_get(vect, i), vect_first(vect), vect->len * vect->size);
   vect->len++;
   return moved;
 }
 
-#include <stdio.h>
-int main() {
-  vect_t *int_vector = vect_typed(int, 10);
-  assert(int_vector->cap == 10 && int_vector->size == 4);
-  assert((size_t)vect_first(int_vector) == (size_t)int_vector + sizeof(vect_t));
-  // push contents
-  int *contents = (int *)vect_first(int_vector);
-  for (int i = 0; i < 10; i++) {
-    contents[i] = i;
-    int_vector->len++;
-  }
-  // printing
-  printf("\n Initial contents ");
-  printIntArray(int_vector, 10);
-  // grow
-  vect_grow_self(int_vector, 10);
-
-  assert(int_vector->cap == 20);
-  vect_move_i(int_vector, 1);
-
-  printf("Lenght: %zu\n", int_vector->len);
-  printf("Last: %p\n", vect_last(int_vector));
-  printf("Index 10: %p\n", vect_get(int_vector, 10));
-  printf("Index len-1: %p\n", vect_get(int_vector, int_vector->len - 1));
-
-  assert(int_vector->len == 11);
-  *((int *)vect_first(int_vector)) = (int)11;
-  *((int *)vect_last(int_vector)) = (int)100;
-
-  int_vector->len = 20;
-  assert(int_vector->len == 20);
-
-  vect_set_auto_grow(1);
-  if (vect_auto_grow == 1 && int_vector->len + 1 > int_vector->cap)
-    puts("needs growing");
-  // print
-  printf("\n Grown Vector");
-  printIntArray(int_vector, 20);
-  printf("capacity %zu\n", int_vector->cap);
-  // pushing 
-  vect_push(int_vector, int, 21);
-  printf("capacity after %zu\n", int_vector->cap);
-  printf("After pushing");
-  printIntArray(int_vector, 21);
-
-  // insert
-  vect_insert(int_vector, int, 55);
-  printf("capacity after %zu\n", int_vector->cap);
-  printf("After pushing");
-  printIntArray(int_vector, 22);
-
-  assert(*vect_get_as(int, int_vector,0) == 55);
-  // push a content but
+void *vect_pushmem(vect_t *vect, void *value) {
+  vect_before_push(vect);
+  memmove(vect_end(vect), value, vect->size);
+  vect->len++;
+  return value;
 }
+
+void *vect_insertmem(vect_t *vect, void *value) {
+  vect_before_insert(vect);
+  memmove(vect_first(vect), value, vect->size);
+  vect->len++;
+  return value;
+}
+
+void *vec_append(vect_t *vec0, vect_t *vec1) {
+  if (vec0->cap <= vec0->len + vec1->len) {
+  }
+  return vec0;
+}
+
+#include <stdio.h>
--- lib/vector/vector.h
+++ lib/vector/vector.h
@@ -1,6 +1,7 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
+
 #ifndef LIB_VECTOR_H
 #define LIB_VECTOR_H
 typedef struct vec_t {
@@ -10,7 +11,7 @@ typedef struct vec_t {
 
 #define VECT_T_SIZE sizeof(vect_t)
 // state 
-#define VECT_STATE_AUTO_GROW vect_auto_grow
+#define VECT_STATE_AUTO_GROW vect_grow_auto()
 // get the first content
 #define vect_first(vect)((char*)((size_t)vect + sizeof(vect_t)))
 // get value of the  vector memebers
@@ -20,7 +21,13 @@ typedef struct vec_t {
 #define vect_end(vect) vect_get(vect, vect->len)
 
 
+/**
+ * Set if the vector should be automatically growing 
+ * @param grow set 1 if you want it to grow
+ */
 void vect_set_auto_grow(int grow);
+/** Seff if the vector is automatically growing **/
+bool vect_grow_auto();
 
 /**
  * allocate a new vector
@@ -36,7 +43,9 @@ vect_t* vect_alloc(size_t size, size_t count);
  * @returns the grown vector
  **/
 vect_t *vect_grow(vect_t *vect, size_t count);
-
+/** Grow vector if needed **/
+size_t vect_grow_if_needed(vect_t *vect); 
+/** return itself when growinig vector **/
 #define  vect_grow_self(vect, count) (vect = vect_grow(vect,count))
 
 /** 
@@ -62,6 +71,18 @@ void* vect_move_i(vect_t* vect, size_t i);
 #define vect_typed(type, cap) vect_alloc(sizeof(type), cap);
 /** get a vector to a certain type **/
 #define vect_get_as(type, vect, i) ((type*)vect_get(vect, i))
+/** check if it is initialize **/
+#define vect_is_init(vect, i) i < vect->len 
+/** get the vector as a value **/
+#define vect_get2(vect, i) *vect_get(vect, i)
+/** same as vect_get2 but has type **/
+#define vect_get_as2(type, vect, i) *((type*)vect_get(vect, i))
+
+
+
+
+
+
 
 // PUSING VALUES
 
@@ -71,8 +92,16 @@ void* vect_move_i(vect_t* vect, size_t i);
  * @param type is the type of the content to push
  * @param value is the value of type `type` to push
  **/
+
+#define  vect_before_insert(vect) vect_grow_if_needed(vect);\
+  vect_move_i(vect,1);\
+  char* content = vect_first(vect);
+
+#define vect_before_push(vect) vect_grow_if_needed(vect)
+
+
 #define vect_push(vect, type, value){\
-  vect_grow_if_needed(vect);\
+  vect_before_push(vect);\
   *((type*)vect_get(vect, vect->len)) = value;\
   vect->len++;\
 }
@@ -89,8 +118,23 @@ void* vect_move_i(vect_t* vect, size_t i);
   vect_grow_if_needed(vect);\
   vect_move_i(vect,1);\
   type* content = ((type*)vect_get(vect, 0));\
-  memmove(vect_first(vect), content, vect->size);\
   *content = value;\
   vect->len++;\
 }
+
+
+/**
+ * Insert a new content to the vector  by moving memeory
+ * @param *vector is the vector to insirt
+ * @param *value is hte memory address to copy from
+ **/
+void* vect_insertmem(vect_t* vect, void* value);
+/*
+ * Push a new content to the vector  by moving memeory
+ * @param *vector is the vector to insirt
+ * @param *value is hte memory address to copy from
+ **/
+void* vect_pushmem(vect_t* vect, void* value);
+
+
 #endif
